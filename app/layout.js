d302bd55481a4610e6e325dc0697926fac6ab6a9
JustPerfectDesigns <gabriel.jpdesigns@gmail.com>
2024-11-12 12:23:01 +0100

    Website Live

--- app/layout.js
+++ app/layout.js
@@ -21,14 +21,14 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <body className={inter.className}>
           <Toaster />
-          {/* <Navigation />
+          <Navigation />
           <div className="lg:hidden py-4 bg-white">
             <MobileNavbar />
-          </div> */}
-          {/* <main className="ui__container mt-12 lg:mt-24"> */}
-          {/* <Navbar /> */}
-          {/* {children} */}
-          <div className="h-screen ui__container flex flex-col justify-center items-center gap-4">
+          </div>
+          <main className="ui__container mt-12 lg:mt-24">
+            {/* <Navbar /> */}
+            {children}
+            {/* <div className="h-screen ui__container flex flex-col justify-center items-center gap-4">
             <h1 className="text-4xl lg:text-8xl tracking-tight text-slate-900 text-center font-bold">
               This Website is currently unavailable till further notice.
             </h1>
@@ -39,11 +39,11 @@ export default function RootLayout({ children }) {
               </a>{" "}
               for more information.
             </p>
-          </div>
-          {/* </main> */}
-          {/* <Footer /> */}
+          </div> */}
+          </main>
+          <Footer />
 
-          {/* <div className="fixed bottom-6 right-6">
+          <div className="fixed bottom-6 right-6">
             <a
               href="https://wa.me/+2348068499883?text=Hello, I need an info from KPearl Couture website."
               target="_blank"
@@ -54,7 +54,7 @@ export default function RootLayout({ children }) {
                 className="w-12 md:w-14 opacity-80"
               />
             </a>
-          </div> */}
+          </div>
         </body>
       </html>
     </ClerkProvider>
